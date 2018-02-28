1ffa35fffc8b3a7c2db4304c2a0017f6bb8e641e
atnimak <atnimak@gmail.com>
2018-02-28 23:05:40 +0300

    Added a simple functional

--- app/src/main/java/com/example/atnimak/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/atnimak/courtcounter/MainActivity.java
@@ -2,12 +2,40 @@ package com.example.atnimak.courtcounter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    int scoreTeamA = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        displayFromTeamA(scoreTeamA);
     }
+
+    public void displayFromTeamA(int score){
+        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
+
+    }
+
+    public void plusThree(View view){
+        scoreTeamA+=3;
+        displayFromTeamA(scoreTeamA);
+    }
+
+    public void plusTwo(View view){
+        scoreTeamA+=2;
+        displayFromTeamA(scoreTeamA);
+    }
+
+    public void freeThrow(View view){
+        scoreTeamA+=1;
+        displayFromTeamA(scoreTeamA);
+
+    }
+
+
 }
