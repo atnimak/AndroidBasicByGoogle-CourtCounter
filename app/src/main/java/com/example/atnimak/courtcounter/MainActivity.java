2e7c8dc96d6652c5ad4806f50d2058615087eeda
atnimak <atnimak@gmail.com>
2018-03-01 18:04:48 +0300

    did some refactoring

--- app/src/main/java/com/example/atnimak/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/atnimak/courtcounter/MainActivity.java
@@ -1,7 +1,7 @@
 package com.example.atnimak.courtcounter;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 
@@ -17,46 +17,47 @@ public class MainActivity extends AppCompatActivity {
         displayFromTeamB(scoreTeamB);
     }
 
-    public void displayFromTeamA(int score){
+    public void displayFromTeamA(int score) {
         TextView scoreView = (TextView) findViewById(R.id.team_a_score);
         scoreView.setText(String.valueOf(score));
 
     }
-    public void displayFromTeamB(int score){
+
+    public void displayFromTeamB(int score) {
         TextView scoreView = (TextView) findViewById(R.id.team_b_score);
         scoreView.setText(String.valueOf(score));
 
     }
 
-    public void plusThreeForTeamA(View view){
-        scoreTeamA+=3;
+    public void plusThreeForTeamA(View view) {
+        scoreTeamA += 3;
         displayFromTeamA(scoreTeamA);
     }
 
-    public void plusTwoForTeamA(View view){
-        scoreTeamA+=2;
+    public void plusTwoForTeamA(View view) {
+        scoreTeamA += 2;
         displayFromTeamA(scoreTeamA);
     }
 
-    public void plusOneForTeamA(View view){
-        scoreTeamA+=1;
+    public void plusOneForTeamA(View view) {
+        scoreTeamA += 1;
         displayFromTeamA(scoreTeamA);
 
     }
 
 
     public void plusThreeForTeamB(View view) {
-        scoreTeamB+=3;
+        scoreTeamB += 3;
         displayFromTeamB(scoreTeamB);
     }
 
     public void plusTwoForTeamB(View view) {
-        scoreTeamB+=2;
+        scoreTeamB += 2;
         displayFromTeamB(scoreTeamB);
     }
 
     public void plusOneForTeamB(View view) {
-        scoreTeamB+=1;
+        scoreTeamB += 1;
         displayFromTeamB(scoreTeamB);
     }
 
