da1caaff7e49c5adf11bfe37c2d54ddd79cbc0b5
atnimak <atnimak@gmail.com>
2018-03-01 14:23:27 +0300

    add functional for team b

--- app/src/main/java/com/example/atnimak/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/atnimak/courtcounter/MainActivity.java
@@ -7,12 +7,14 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     int scoreTeamA = 0;
+    int scoreTeamB = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         displayFromTeamA(scoreTeamA);
+        displayFromTeamB(scoreTeamB);
     }
 
     public void displayFromTeamA(int score){
@@ -20,22 +22,41 @@ public class MainActivity extends AppCompatActivity {
         scoreView.setText(String.valueOf(score));
 
     }
+    public void displayFromTeamB(int score){
+        TextView scoreView = (TextView) findViewById(R.id.team_b_score);
+        scoreView.setText(String.valueOf(score));
 
-    public void plusThree(View view){
+    }
+
+    public void plusThreeForTeamA(View view){
         scoreTeamA+=3;
         displayFromTeamA(scoreTeamA);
     }
 
-    public void plusTwo(View view){
+    public void plusTwoForTeamA(View view){
         scoreTeamA+=2;
         displayFromTeamA(scoreTeamA);
     }
 
-    public void freeThrow(View view){
+    public void plusOneForTeamA(View view){
         scoreTeamA+=1;
         displayFromTeamA(scoreTeamA);
 
     }
 
 
+    public void plusThreeForTeamB(View view) {
+        scoreTeamB+=3;
+        displayFromTeamB(scoreTeamB);
+    }
+
+    public void plusTwoForTeamB(View view) {
+        scoreTeamB+=2;
+        displayFromTeamB(scoreTeamB);
+    }
+
+    public void plusOneForTeamB(View view) {
+        scoreTeamB+=1;
+        displayFromTeamB(scoreTeamB);
+    }
 }
